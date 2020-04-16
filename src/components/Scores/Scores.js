d4264fa745becc9aa08a37b7b3bf131155507e86
marcinolszewski <marcin.olszewski@icloud.com>
2020-04-16 17:25:42 +0200

    added scores

--- src/components/Scores/Scores.js
+++ src/components/Scores/Scores.js
@@ -1,32 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { SettingsContext } from '../../context/SettingsContext';
+import styles from './Scores.module.scss';
 import '../../Globals.scss';
 import * as firebase from 'firebase/app';
-import 'firebase/database';
+import Spinner from '../Spinner/Spinner';
 
 const Scores = ({ showMenu }) => {
-  const [scores, setScores] = useState(0);
-
+  const [scores, setScores] = useState([]);
+  const [spinner, toggleSpinner] = useState({ spinnerVisible: true });
+  const context = useContext(SettingsContext);
   const database = firebase.database();
 
-  const data = database
-    .ref('scores')
-    .once('value')
-    .then((snapshot) => {
-      const data = snapshot.val();
-      const sortedData = Object.keys(data)
-        .sort((a, b) => data[a].steps - data[b].steps)
-        .map((key) => data[key]);
-
-      sortedData.map((el) => setScores({ name: el.name, score: el.score }));
-    });
+  useEffect(() => {
+    database
+      .ref('scores')
+      .once('value')
+      .then((snapshot) => {
+        const data = snapshot.val();
+        const sortedData = Object.keys(data)
+          .sort((a, b) => data[a].steps - data[b].steps)
+          .map((key) => data[key]);
+        toggleSpinner({ spinnerVisible: false });
+        setScores(sortedData);
+      });
+  }, []);
 
   return (
     <div className="menu__wrapper">
-      <ul className="menu">
+      <div className="menu">
         <button className="menu__btnBack" onClick={() => showMenu()}>
           BACK
         </button>
-      </ul>
+        <table className={styles.table}>
+          <thead>
+            <tr className={styles.table__header}>
+              <th>No:</th>
+              <th>Name:</th>
+              <th>Steps:</th>
+            </tr>
+          </thead>
+          <tbody>
+            {scores.map((el, idx) => {
+              return (
+                <tr
+                  key={idx}
+                  className={`${styles.table__row} ${
+                    context.gameBackground === 'dark' ? styles.dark : ''
+                  }`}
+                >
+                  <td>{idx + 1}.</td>
+                  <td>{el.name}</td>
+                  <td>{el.steps}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        {spinner.spinnerVisible ? <Spinner /> : null}
+      </div>
     </div>
   );
 };
