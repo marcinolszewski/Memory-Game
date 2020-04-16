bf2afa57bb0a642fbdd40e16823400a1a6079f7d
marcinolszewski <marcin.olszewski@icloud.com>
2020-04-16 10:03:51 +0200

    added firebase

--- src/components/Scores/Scores.js
+++ src/components/Scores/Scores.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import '../../Globals.scss';
+import * as firebase from 'firebase/app';
+import 'firebase/database';
 
 const Scores = ({ showMenu }) => {
+  const database = firebase.database();
+
+  const data = database
+    .ref('scores')
+    .once('value')
+    .then((snapshot) => {
+      const data = snapshot.val();
+      const sortedData = Object.keys(data)
+        .sort((a, b) => data[a].steps - data[b].steps)
+        .map((key) => data[key]);
+
+      sortedData.map((el) => console.log(el.name, el.steps));
+    });
+
   return (
     <div className="menu__wrapper">
       <ul className="menu">
         <button className="menu__btnBack" onClick={() => showMenu()}>
           BACK
         </button>
-        <li>ALA</li>
-        <li>OLA</li>
-        <li>ZOS</li>
-        <li>ZUZ</li>
-        <li>MAR</li>
       </ul>
     </div>
   );
