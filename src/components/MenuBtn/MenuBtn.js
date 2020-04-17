e08dc3f515e29e9deaf04111536f2ec2b37cf4a9
marcinolszewski <marcin.olszewski@icloud.com>
2020-04-17 12:26:25 +0200

    refactored menu buttons

--- src/components/MenuBtn/MenuBtn.js
+++ src/components/MenuBtn/MenuBtn.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import styles from './MenuBtn.module.scss';
 
-const MenuBtn = (props) => {
+const MenuBtn = ({ btnStyle, handleOnClick, children }) => {
+  let btnTheme;
+
+  switch (btnStyle) {
+    case 'back':
+      btnTheme = styles.menu__returnBtn;
+      break;
+    case 'special':
+      btnTheme = styles.menu__specialBtn;
+      break;
+    default:
+      btnTheme = styles.menu__btn;
+      break;
+  }
   return (
-    <button className={styles.menu__btn} onClick={props.handleOnClick}>
-      {props.children}
+    <button className={btnTheme} onClick={handleOnClick}>
+      {children}
     </button>
   );
 };
