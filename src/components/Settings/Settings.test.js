5f5f0ef087ec5186b21f10438a34b239b7fabd1f
marcinolszewski <marcin.olszewski@icloud.com>
2020-04-23 21:18:27 +0200

    still wrong

--- src/components/Settings/Settings.test.js
+++ src/components/Settings/Settings.test.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import Settings from './Settings';
+import SettingsContextProvider from './../../context/SettingsContext';
 import MenuBtn from './../MenuBtn/MenuBtn';
-import SettingsContext from './../../context/SettingsContext';
 
 describe('Component <Settings />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(<Settings />, {
+      wrappingComponent: SettingsContextProvider,
+    });
+  });
+
   it('should render itself', () => {
     shallow(<Settings />);
   });
 
-  it('Should contain 6 buttons', () => {
-    const wrapper = mount(<Settings />);
+  it('renders settings buttons', () => {
+    expect(wrapper.find('button')).toHaveLength(6);
+  });
 
-    expect(wrapper.find(MenuBtn)).toHaveLength(6);
+  it('renders default values', () => {
+    SettingsContextProvider.setState({ name: 'PlayerCHUJ' });
+
+    console.log(wrapper.debug());
+    expect(wrapper.text().includes('Player')).toBe(true);
   });
 });
